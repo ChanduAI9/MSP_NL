13ba256d9b637d6047694ce77a92cc13a5a1f7f0
ChanduAI9 <namburichandu65@gmail.com>
2024-02-12 11:55:27 +0530

    Updated one

---
+++ NumberLeader/user_auth/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path,include
+from .views import UserRegistrationView,UserLoginView,FinancialDetailsView,home,ForecastedFinancialDetailsView,CalculateMetricsView,DownloadTopCompaniesExcelView,FinancialDataView, FinancialDataScreenerView,CustomUserView,CustomView
+# from rest_framework.routers import Defaenv\SultRouter
+
+# router=DefaultRouter()
+# router.register(r'financial-data',FinancialDataViewSet,basename='financial-data')
+
+
+urlpatterns = [
+
+    path('financial-data/query/', FinancialDataView.as_view(), name='financial_data_query'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('', UserLoginView.as_view(), name='login'),
+    # path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('financial-details/', FinancialDetailsView.as_view(template_name='form3.html'), name='financial-details'),
+    path('home/',home, name='home'),
+    path('forecasted-financial-details/', ForecastedFinancialDetailsView.as_view(template_name='form3.html'), name='forecasted-financial-details'),
+    # path('download/', FinancialDataView.as_view, name='download'),
+    path('calculate-metrics/', CalculateMetricsView.as_view(), name='calculate-metrics'),
+    path('download-top-metrics/',DownloadTopCompaniesExcelView.as_view(), name='download_metrics'),
+    # path('financial-data/',include(router.urls)),
+    # path('import-excel/', ExcelImportView.as_view(), name='excel-import'),
+    path('financial-data/screener/', FinancialDataScreenerView.as_view(), name='financial_data_screener'),
+    path('custom-user/',CustomUserView.as_view(),name='custom-user-detail'),
+    path('custom-user/view',CustomView.as_view(),name='custom'),
+]
